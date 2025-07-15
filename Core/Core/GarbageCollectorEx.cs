7e9868a9196d8c393429104397b36b50f213cdad
Andreas Börcsök <a.boercsoek@outlook.de>
2025-07-15 10:43:47 +0200

    Removed .NET Version from Project- und Solution-File. Added more Core-Classes and CUI-Classes.

---
+++ Core/Core/GarbageCollectorEx.cs
@@ -0,0 +1,40 @@
+﻿//--------------------------------------------------------------------------
+// File:    GarbageCollectorEx.cs
+// Content:	Implementation of class GarbageCollectorEx
+// Author:	Andreas Börcsök
+//--------------------------------------------------------------------------
+#region Using directives
+
+#endregion
+
+namespace AnBo.Core
+{
+    /// <summary>
+	/// Garbage Collector (<see cref="GC"/>) extensions.
+	/// </summary>
+	/// <seealso cref="GC"/>
+	public static class GarbageCollectorEx
+    {
+        /// <summary>
+        /// Collects and waits for pending finalizers several times
+        /// </summary>
+        public static void AggressiveCollect()
+        {
+            AggressiveCollect(4);
+        }
+
+        /// <summary>
+        /// Collects and waits for pending finalizers <param name="iterations" />
+        /// times
+        /// </summary>
+        public static void AggressiveCollect(int iterations)
+        {
+            for (int i = 0; i < iterations; i++)
+            {
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+            }
+        }
+
+    }
+}
---
+++ Core/DataTypes/Converters/HexStringFormatOptions.cs
@@ -0,0 +1,34 @@
+﻿//--------------------------------------------------------------------------
+// File:    HexStringFormatOptions.cs
+// Content:	Implementation of a hex converter options enum
+// Author:	Andreas Börcsök
+//--------------------------------------------------------------------------
+#region Using directives
+
+#endregion
+
+namespace AnBo.Core
+{
+    /// <summary>
+	/// ToHexString format options
+	/// </summary>
+	public enum HexStringFormatOptions
+    {
+        /// <summary>
+        /// No format options
+        /// </summary>
+        None,
+        /// <summary>
+        /// Add 0x as prefix to the hex string
+        /// </summary>
+        AddZeroXPrefix,
+        /// <summary>
+        /// Insert a separator between the hex byte strings (default is space).
+        /// </summary>
+        AddSeparatorBetweenHexBytes,
+        /// <summary>
+        /// Insert a Space between the hex byte strings and add a new line every 16 hex byte strings.
+        /// </summary>
+        AddNewLineAfter16HexBytes,
+    }
+}
