bf89213f3931b9875d7cd622531d04a7461b8f36
Andreas Börcsök <a.boercsoek@outlook.de>
2025-07-15 07:29:43 +0200

    Moved Type helper classes in Reflection folder.

---
+++ Core/CUI/AppMenuController.cs
@@ -0,0 +1,21 @@
+﻿//--------------------------------------------------------------------------
+// File:    AppMenuController.cs
+// Content:	Implementation of class AppMenuController
+// Author:	Andreas Börcsök
+//--------------------------------------------------------------------------
+#region Using directives
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#endregion
+
+namespace AnBo.Core.CUI
+{
+    internal class AppMenuController
+    {
+    }
+}
